652ea508ff63c43bb3818783b9267b828b20d5d7
= <=>
2014-05-12 12:12:29 -0400

    initialize neighbors for all tiles

--- minesweeper.rb
+++ minesweeper.rb
@@ -20,15 +20,48 @@ class Board
       end
     end
     
-    seed_board
+    build_neighbors
+    
+    #seed_board
+    
+    #set_neighbor_bomb_counts
     
     p @board
     
   end
   
+  def build_neighbors
+    
+    @height.times do |x|
+      @width.times do |y|
+        tile = @board[x][y]
+        tile.neighbors = get_neighbors(tile)
+      end
+    end
+     
+  end
+  
+  def get_neighbors(tile)
+    deltas = [ [-1,-1], [0,-1], [1,-1],
+               [-1,0],          [1,0],
+               [-1, 1], [0,1],  [1,1]]
+               
+    neighbors = deltas.map do |delta|
+      [tile.coords.first + delta.first,
+        tile.coords.last + delta.last]
+    end
+    
+    neighbors.select do |neighbor|
+      neighbor.first >= 0 && neighbor.last >= 0 &&
+       neighbor.first < @width && neighbor.last < @height
+    end
+    
+  end
+   
   def seed_board
     bomb_count = 0
     until bomb_count == @total_bombs
+      # stop seeding if all tiles == bombs
       break if bomb_count == @height * @width
       # Get random coordinate
       x , y = rand(0...@width), rand(0...@height)
@@ -44,11 +77,12 @@ end
 class Tile
   
   attr_reader :coords 
-  attr_accessor :bomb
+  attr_accessor :bomb, :neighbors
   
   def initialize(coords)
     @coords = coords
     @bomb = false
+    @neighbors = []
   end
   
 end
